faaceb3b9b6fe68724f5ee85ee379b798ae0f634
manibaddireddy7571@gmail.com <manibaddireddy7571@gmail.com>
2023-04-17 06:39:29 +0000

    added git notebook

---
+++ gittry.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC CREATE TABLE student3(id int,name varchar(30),brach varchar(30));
+# MAGIC insert into student3 values (1,"harsha","mech"),(2,"lokesh","civil");
