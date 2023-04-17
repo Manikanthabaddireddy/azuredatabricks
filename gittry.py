7b19a8f8f16a8632e4bab11872009ff7b468f95a
manibaddireddy7571@gmail.com <manibaddireddy7571@gmail.com>
2023-04-17 06:45:52 +0000

    updated id 1

--- gittry.py
+++ gittry.py
@@ -2,3 +2,8 @@
 # MAGIC %sql
 # MAGIC CREATE TABLE student3(id int,name varchar(30),brach varchar(30));
 # MAGIC insert into student3 values (1,"harsha","mech"),(2,"lokesh","civil");
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC update student3 set id=id+1 where id=1;
